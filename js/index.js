789fe6a3ede5e0272f2e16b639a45709f285ed49
Inna cherkashina <cherkashina.i@vilavi.com>
2024-06-27 17:18:06 +0700

    Подправлен скрипт вертикального скролла, добавлена секция со слайдером, секция программаБ секция место встречи

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
+//! Слайдер Start
+$(document).ready(function () {
+  $('.slider-container').slick({
+    arrows: true,
+    adaptiveHeight: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    // infinite: true,
+    // autoplay: true,
+    // autoplaySpeed: 1000
+  });
+});
+
+//! Слайдер End
+
+//~ Запуск видеоплеера Start
 let videoElement = document.querySelector('video');
 let buttonVideo = document.querySelector('.btn-video')
 buttonVideo.addEventListener('click', function () {
@@ -11,56 +27,99 @@ buttonVideo.addEventListener('click', function () {
     buttonVideo.classList.add('video-play');
   }
 });
+//~ Запуск видеоплеера End
 
 
+//^ Анимация смены цвета ползунка при вертикальном скролле Start
 let colorElement = document.querySelector('.circle-scroll svg');
 window.addEventListener('scroll', () => {
-  if (window.pageYOffset < 3800) {
+  if (window.pageYOffset < 3308) {
     colorElement.classList.remove('circle-scroll--color-one');
   }
-  else if (window.pageYOffset > 3800 && window.pageYOffset < 3924) {
+  else if (window.pageYOffset > 3308 && window.pageYOffset < 3348) {
     colorElement.classList.add('circle-scroll--color-one');
     colorElement.classList.remove('circle-scroll--color-two');
   }
-  else if (window.pageYOffset > 3924 && window.pageYOffset < 3970) {
+  else if (window.pageYOffset > 3348 && window.pageYOffset < 3378) {
     colorElement.classList.remove('circle-scroll--color-one');
     colorElement.classList.add('circle-scroll--color-two');
     colorElement.classList.remove('circle-scroll--color-three');
   }
-  else if (window.pageYOffset > 3970 && window.pageYOffset < 4160) {
+  else if (window.pageYOffset > 3378 && window.pageYOffset < 3428) {
     colorElement.classList.remove('circle-scroll--color-two');
     colorElement.classList.add('circle-scroll--color-three');
     colorElement.classList.remove('circle-scroll--color-four');
   }
-  else if (window.pageYOffset > 4160 && window.pageYOffset < 4500) {
+  else if (window.pageYOffset > 3428 && window.pageYOffset < 3460) {
     colorElement.classList.remove('circle-scroll--color-three');
     colorElement.classList.add('circle-scroll--color-four');
+    colorElement.classList.remove('circle-scroll--color-five');
+  }
+  else if (window.pageYOffset > 3460 && window.pageYOffset < 3500) {
+    colorElement.classList.remove('circle-scroll--color-four');
+    colorElement.classList.add('circle-scroll--color-five');
+    colorElement.classList.remove('circle-scroll--color-six');
+  }
+  else if (window.pageYOffset > 3500 && window.pageYOffset < 3630) {
+    colorElement.classList.remove('circle-scroll--color-five');
+    colorElement.classList.add('circle-scroll--color-six');
+    colorElement.classList.remove('circle-scroll--color-seven');
 
   }
-
+  else if (window.pageYOffset > 3630 && window.pageYOffset < 3700) {
+    colorElement.classList.remove('circle-scroll--color-six');
+    colorElement.classList.add('circle-scroll--color-seven');
+    colorElement.classList.remove('circle-scroll--color-eight');
+  }
+  else if (window.pageYOffset > 3700 && window.pageYOffset < 4000) {
+    colorElement.classList.remove('circle-scroll--color-seven');
+    colorElement.classList.add('circle-scroll--color-eight');
+  }
 });
 
 let meetColorElement = document.querySelector('.meet-place__circle-scroll svg');
 window.addEventListener('scroll', () => {
-  if (window.pageYOffset < 6870) {
+  if (window.pageYOffset < 6350) {
     meetColorElement.classList.remove('meet-place__circle-scroll--color-one');
   }
-  else if (window.pageYOffset > 6870 && window.pageYOffset < 6940) {
+  else if (window.pageYOffset > 6350 && window.pageYOffset < 6390) {
     meetColorElement.classList.add('meet-place__circle-scroll--color-one');
     meetColorElement.classList.remove('meet-place__circle-scroll--color-two');
   }
-  else if (window.pageYOffset > 6940 && window.pageYOffset < 7010) {
+  else if (window.pageYOffset > 6390 && window.pageYOffset < 6430) {
     meetColorElement.classList.remove('meet-place__circle-scroll--color-one');
     meetColorElement.classList.add('meet-place__circle-scroll--color-two');
     meetColorElement.classList.remove('meet-place__circle-scroll--color-three');
   }
-  else if (window.pageYOffset > 7010 && window.pageYOffset < 7120) {
+  else if (window.pageYOffset > 6430 && window.pageYOffset < 6470) {
     meetColorElement.classList.remove('meet-place__circle-scroll--color-two');
     meetColorElement.classList.add('meet-place__circle-scroll--color-three');
     meetColorElement.classList.remove('meet-place__circle-scroll--color-four');
   }
-  else if (window.pageYOffset > 7120 && window.pageYOffset < 8500) {
+  else if (window.pageYOffset > 6470 && window.pageYOffset < 6536) {
     meetColorElement.classList.remove('meet-place__circle-scroll--color-three');
     meetColorElement.classList.add('meet-place__circle-scroll--color-four');
+    meetColorElement.classList.remove('meet-place__circle-scroll--color-five');
+  }
+  else if (window.pageYOffset > 6536 && window.pageYOffset < 6580) {
+    meetColorElement.classList.remove('meet-place__circle-scroll--color-four');
+    meetColorElement.classList.add('meet-place__circle-scroll--color-five');
+    meetColorElement.classList.remove('meet-place__circle-scroll--color-six');
+  }
+  else if (window.pageYOffset > 6580 && window.pageYOffset < 6650) {
+    meetColorElement.classList.remove('meet-place__circle-scroll--color-five');
+    meetColorElement.classList.add('meet-place__circle-scroll--color-six');
+    meetColorElement.classList.remove('meet-place__circle-scroll--color-seven');
+  }
+  else if (window.pageYOffset > 6650 && window.pageYOffset < 6720) {
+    meetColorElement.classList.remove('meet-place__circle-scroll--color-six');
+    meetColorElement.classList.add('meet-place__circle-scroll--color-seven');
+    meetColorElement.classList.remove('meet-place__circle-scroll--color-eight');
+  }
+  else if (window.pageYOffset > 6720 && window.pageYOffset < 8000) {
+    meetColorElement.classList.remove('meet-place__circle-scroll--color-seven');
+    meetColorElement.classList.add('meet-place__circle-scroll--color-eight');
   }
 });
+//^ Анимация смены цвета ползунка при вертикальном скролле End
+
