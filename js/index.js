5a22b3188cc3851d682800caf96e8325153623b0
Inna cherkashina <cherkashina.i@vilavi.com>
2024-06-27 09:16:23 +0700

    добавлена новая секция Где встречаемся, скрипт, стили и картинка к ней

--- js/index.js
+++ js/index.js
@@ -12,7 +12,8 @@ buttonVideo.addEventListener('click', function () {
   }
 });
 
-let colorElement = document.querySelector('.circle-scroll svg')
+
+let colorElement = document.querySelector('.circle-scroll svg');
 window.addEventListener('scroll', () => {
   if (window.pageYOffset < 3800) {
     colorElement.classList.remove('circle-scroll--color-one');
@@ -32,9 +33,34 @@ window.addEventListener('scroll', () => {
     colorElement.classList.remove('circle-scroll--color-four');
   }
   else if (window.pageYOffset > 4160 && window.pageYOffset < 4500) {
-    colorElement.classList.remove('circle-scroll--color-three');    
+    colorElement.classList.remove('circle-scroll--color-three');
     colorElement.classList.add('circle-scroll--color-four');
 
   }
 
+});
+
+let meetColorElement = document.querySelector('.meet-place__circle-scroll svg');
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset < 6870) {
+    meetColorElement.classList.remove('meet-place__circle-scroll--color-one');
+  }
+  else if (window.pageYOffset > 6870 && window.pageYOffset < 6940) {
+    meetColorElement.classList.add('meet-place__circle-scroll--color-one');
+    meetColorElement.classList.remove('meet-place__circle-scroll--color-two');
+  }
+  else if (window.pageYOffset > 6940 && window.pageYOffset < 7010) {
+    meetColorElement.classList.remove('meet-place__circle-scroll--color-one');
+    meetColorElement.classList.add('meet-place__circle-scroll--color-two');
+    meetColorElement.classList.remove('meet-place__circle-scroll--color-three');
+  }
+  else if (window.pageYOffset > 7010 && window.pageYOffset < 7120) {
+    meetColorElement.classList.remove('meet-place__circle-scroll--color-two');
+    meetColorElement.classList.add('meet-place__circle-scroll--color-three');
+    meetColorElement.classList.remove('meet-place__circle-scroll--color-four');
+  }
+  else if (window.pageYOffset > 7120 && window.pageYOffset < 8500) {
+    meetColorElement.classList.remove('meet-place__circle-scroll--color-three');
+    meetColorElement.classList.add('meet-place__circle-scroll--color-four');
+  }
 });
