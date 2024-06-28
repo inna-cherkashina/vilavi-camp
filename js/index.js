f019fbf652e9cffe817bcf0f13eddbb3090a5c34
Inna cherkashina <cherkashina.i@vilavi.com>
2024-06-28 11:41:45 +0700

    добавлен скрипт анимации для тумблеров

--- js/index.js
+++ js/index.js
@@ -126,21 +126,62 @@ window.addEventListener('scroll', () => {
 
 //TODO Аккордион Start
 let accordeons = document.querySelectorAll('.accordion-item');
- accordeons.forEach(function (accord) {
+accordeons.forEach(function (accord) {
   accord.addEventListener('click', function (elem) {
-  
+
     let action = elem.target;
     let currentAccordElem = action.closest('.accordion-item');//определяем текущий бокс
     let currentContent = action.nextElementSibling; // находим скрытый контент
     currentAccordElem.classList.toggle("active"); //присваиваем основному контейнеру флаг активности   
-    if (currentAccordElem.classList.contains("active")) { 
+    if (currentAccordElem.classList.contains("active")) {
       currentContent.style.maxHeight = currentContent.scrollHeight + "px";
       //Обратить внимание чтоб стили были присвоены классу .accordion-collapse
 
     }
     else {
-      currentContent.style.maxHeight = 0;       
+      currentContent.style.maxHeight = 0;
     }
   });
- });
+});
 //TODO Аккордион End
+
+let animItems = document.querySelectorAll('.anim-items');
+
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll)
+  function animOnScroll() {
+    for (let i = 0; i < animItems.length; i++) {
+      let animItem = animItems[i];
+      let animItemHeight = animItem.offsetHeight;
+      let animItemOffset = offset(animItem).top;
+      let animStart = 1000;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if ((window.scrollY > animItemOffset - animItemPoint) && window.scrollY < (animItemOffset + animItemHeight)) {
+        animItem.classList.add('active');
+      } else {
+
+        if (!animItem.classList.contains('anim-no-hide')) {
+          animItem.classList.remove('active');
+        }
+        //а если мы хотим чтоб анимация сохранялась при скролле вверх и вниз, то оставляем просто
+        // else {animItem.classList.remove('active');}
+      }
+    }
+  }
+  function offset(elem) {
+    let rect = elem.getBoundingClientRect();
+    let scrollLeft = window.scrollX || document.documentElement.scrollLeft;
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    return {
+      top: rect.top + scrollTop,
+      left: rect.left + scrollLeft
+    }
+  }
+  animOnScroll();
+}
